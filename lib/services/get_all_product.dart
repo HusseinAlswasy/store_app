b1fe9e5a7eaf3d8c81817b471c3cc2de2e496310
Hussein Alswasy <01096694296hud@gmail.com>
2023-05-01 21:03:26 +0300

    get all product services

---
+++ lib/services/get_all_product.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:store_app/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class GetAllProduct {
+  Future<List<ProductModel>> getAllProduct() async {
+    http.Response response = await http
+        .get(Uri.parse('https://fakestoreapi.com/products')); //response
+
+    List<dynamic> data = jsonDecode(response.body); //decode
+
+    List<ProductModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
+      productList.add(ProductModel.fromJson(data[i]));
+    }
+    return productList;
+  }
+}
