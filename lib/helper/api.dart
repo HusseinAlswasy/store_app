fe4c8d7c8ac962fc568ac2fa452e7182613d91b5
Hussein Alswasy <01096694296hud@gmail.com>
2023-05-06 00:25:11 +0300

    finish all logic

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,9 +1,16 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
-    http.Response response = await http.get(Uri.parse(url));
+  Future<dynamic> get({required String url,@required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+    http.Response response = await http.get(Uri.parse(url),headers: headers);
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
@@ -11,5 +18,48 @@ class Api {
       throw Exception(
           'there is a problem with status code ${response.statusCode}');
     }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data;
+  }
+
+  Future<dynamic> put(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    headers.addAll({
+      'Content-Type':'application/x-www-form-urlencoded',
+    });
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () async {
+          http.Response response = await http
+              .post(Uri.parse('https://fakestoreapi.com/products'), body: {
+            'title': 'test product',
+            'price': '13.5',
+            'description': 'lorem ipsum set',
+            'image': 'https://i.pravatar.cc',
+            'category': 'electronic',
+          }, headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          });
+        }),
+      ),
     );
   }
 }
