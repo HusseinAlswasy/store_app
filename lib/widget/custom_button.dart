3a3523bd7069376464c4658e4baea6823624ae71
Hussein Alswasy <01096694296hud@gmail.com>
2023-05-08 13:14:39 +0300

    create custom button and text form field

---
+++ lib/widget/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  CustomButton({super.key, required this.text,required this.onTap});
+
+  @override
+  String text;
+  Function() onTap;
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        width: double.infinity,
+        height: 60,
+        child: Center(
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/custom_text_field.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField({super.key});
+
+  @override
+  Function(String)? onChanged;
+  bool? obscureText = false;
+  String? hintText;
+  String? labelText;
+
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscureText!,
+      validator: (data) {
+        if (data!.isEmpty) {
+          return 'field is Required';
+        }
+      },
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: labelText,
+        hintStyle: TextStyle(
+          color: Colors.black,
+        ),
+        labelStyle: TextStyle(
+          color: Colors.black,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        // border: OutlineInputBorder(
+        //   borderSide: BorderSide(
+        //     color: Colors.black,
+        //   ),
+        //   borderRadius: BorderRadius.circular(16),
+        // ),
+      ),
+    );
+  }
+}
